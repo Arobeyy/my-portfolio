135cf26286f990e15965c81d3c6e2b3160808b47
arobey <sumiyaakter.arobey@gmail.com>
2024-11-16 01:02:02 +0600

    project video added

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-function toggleMenu () {
+function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open")
-  icon.classList.toggle("open")
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
 }
+
+const videos = document.querySelectorAll(".project-video");
+
+function autoPlayVideo() {
+  videos.forEach((video) => {
+    const rect = video.getBoundingClientRect();
+    if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
+      video.play();
+    } else {
+      video.pause();
+    }
+  });
+}
+
+window.addEventListener("scroll", autoPlayVideo);
+window.addEventListener("resize", autoPlayVideo);
